987e2a26cff4db501836b347a8b41f82a5d88809
deep0294 <deepakagithub@gmail.com>
2024-01-15 17:17:46 +0000

    [Test] Add D585S All Profile Test

--- realsense2_camera/test/live_camera/test_camera_all_profile_tests.py
+++ realsense2_camera/test/live_camera/test_camera_all_profile_tests.py
@@ -142,6 +142,7 @@ machines that don't have the D455 connected.
 '''
 @pytest.mark.skipif (os.getenv('RS_ROS_REGRESSION', "not found") == "not found",reason="Regression is not enabled, define RS_ROS_REGRESSION")
 @pytest.mark.parametrize("launch_descr_with_parameters", [
+    pytest.param(test_params_all_profiles_d455, marks=pytest.mark.d585S),
     pytest.param(test_params_all_profiles_d455, marks=pytest.mark.d455),
     pytest.param(test_params_all_profiles_d415, marks=pytest.mark.d415),
     pytest.param(test_params_all_profiles_d435, marks=pytest.mark.d435),]
